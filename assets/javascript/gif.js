e009202e497771e0baa7364c415f9c418e11195d
Brandan Richardson <brandanrichardson@Brandans-MacBook-Pro.local>
2018-01-30 06:42:49 -0800

    commit 1/30/18 6:42 AM

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,10 +1,52 @@
 // Global variables
-var topics = ["kobe+slam+dunks", "shawn+kemp+slam+dunks", "vince+carter+slam+dunks"];
-console.log(topics);
+var slamDunks = ["kobe bryant", "shawn kemp", "vince carter"];
+console.log(slamDunks);
 
 
+// Function for displaying topics data
+      function renderButtons() {
 
-$("<button>").attr()
+        // Deleting the movie buttons prior to adding new movie buttons
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#dunker").empty();
+
+        // Create a button for each index of the array
+		for(i = 0; i < slamDunks.length; i++) {
+
+          // Then dynamicaly generating buttons for each movie in the array.
+          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+          var newButton = $("<button>");
+          // Adding a class
+          newButton.addClass("baller");
+          // Adding a data-attribute with a value of the movie at index i
+          newButton.attr("data-person", slamDunks[i]);
+          // Providing the button's text with a value of the movie at index i
+          newButton.text(slamDunks[i]);
+          // Adding the button to the HTML
+          $("#dunker").append(newButton);
+        }
+      }
+
+      renderButtons();
+
+      // // This function handles events where one button is clicked
+      // $("#add-movie").on("click", function(event) {
+      //   // event.preventDefault() prevents the form from trying to submit itself.
+      //   // We're using a form so that the user can hit enter instead of clicking the button if they want
+      //   event.preventDefault();
+
+      //   // This line will grab the text from the input box
+      //   var movie = $("#movie-input").val().trim();
+      //   // The movie from the textbox is then added to our array
+      //   movies.push(movie);
+      //   $("#movie-input").val('');
+
+      //   // calling renderButtons which handles the processing of our movie array
+      //   renderButtons();
+      // });
+
+      // // Calling the renderButtons function at least once to display the initial list of movies
+      // renderButtons();
 
 
 
@@ -15,13 +57,14 @@ $("<button>").attr()
 
       // Constructing a URL to search Giphy for the name of the person who said the quote
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        dunker + "&api_key=D9qYKN8gzFg59ImLbNl2v4OW611JJq7e&limit=25";
+        dunker + "slam+dunks" + "&api_key=D9qYKN8gzFg59ImLbNl2v4OW611JJq7e&limit=10";
 
       // Performing our AJAX GET request
       $.ajax({
           url: queryURL,
           method: "GET"
         })
+
         // After the data comes back from the API
         .then(function(response) {
           // Storing an array of results in the results variable
@@ -53,7 +96,7 @@ $("<button>").attr()
               gifDiv.append(personImage);
 
               // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-              $("#gifs-appear-here").prepend(gifDiv);
+              $("#gifs-please").prepend(gifDiv);
             }
           }
         });
